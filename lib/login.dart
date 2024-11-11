b4a88d89b8b8713ee9d767203bf1d4e36518e06b
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-12 00:09:56 +0700

    flutter pub add fluttertoast

---
+++ lib/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
