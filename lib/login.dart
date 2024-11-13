ea4821a670251dfd2b1a73fd64dc052fc60b27ed
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-13 20:57:48 +0700

    mainpage

--- lib/login.dart
+++ lib/login.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tugas_kelompok_minggu7/mainpage.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+final userNameController = TextEditingController();
 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/background_1.svg',
+            alignment: Alignment.bottomCenter,
+            width: 2560.0,
+            height: 1440.0,
+            fit: BoxFit.scaleDown,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Aplikasi Biodata',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Username'),
+                        controller: userNameController,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              print(userNameController.text);
+                              if (userNameController.text ==
+                                  "Mohammad Usman Asegaf") {
+                                Navigator.pushReplacement(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const MainPage();
+                                }));
+                              } else if (userNameController.text == "") {
+                                Fluttertoast.showToast(
+                                    msg: "Username Tidak Boleh Kosong",
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.CENTER,
+                                    timeInSecForIosWeb: 2,
+                                    backgroundColor: Colors.red,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0);
+                              } else
+                                (Fluttertoast.showToast(
+                                    msg: "Username Tidak Terdaftar",
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.CENTER,
+                                    timeInSecForIosWeb: 2,
+                                    backgroundColor: Colors.red,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(150, 50)),
+                            child: const Text(
+                              "Masuk",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            )),
+                      ),
+                    ],
+                  )),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/mainpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tugas_kelompok_minggu7/login.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
