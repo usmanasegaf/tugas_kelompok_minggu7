d57b3ecd3708674066aaf03372fb622c20651479
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-15 01:53:54 +0700

    Login, Main Botttom Nav With Pages

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:tugas_kelompok_minggu7/mainpage.dart';
+import 'package:tugas_kelompok_minggu7/main.dart';
 
 final userNameController = TextEditingController();
 final passwordController = TextEditingController();
@@ -53,34 +53,35 @@ class LoginPage extends StatelessWidget {
                               print(userNameController.text);
                               if (userNameController.text ==
                                       "Mohammad Usman Asegaf" &&
-                                  passwordController.text == "21552011174") {
+                                  passwordController.text == "2155") {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(builder: (context) {
-                                  return const MainPage();
+                                  return HomeScreen();
                                 }));
-                              } else if (userNameController.text == "2" &&
-                                  passwordController.text == "222") {
+                              } else if (userNameController.text ==
+                                      "Syamsu Nuryaman" &&
+                                  passwordController.text == "2255") {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(builder: (context) {
-                                  return const MainPage();
+                                  return HomeScreen();
                                 }));
                               } else if (userNameController.text == "3" &&
                                   passwordController.text == "333") {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(builder: (context) {
-                                  return const MainPage();
+                                  return HomeScreen();
                                 }));
                               } else if (userNameController.text == "4" &&
                                   passwordController.text == "444") {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(builder: (context) {
-                                  return const MainPage();
+                                  return HomeScreen();
                                 }));
                               } else if (userNameController.text == "5" &&
                                   passwordController.text == "555") {
                                 Navigator.pushReplacement(context,
                                     MaterialPageRoute(builder: (context) {
-                                  return const MainPage();
+                                  return HomeScreen();
                                 }));
                               } else if (userNameController.text == "" ||
                                   passwordController.text == "") {
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,124 @@ import 'package:flutter/material.dart';
 import 'package:tugas_kelompok_minggu7/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: LoginPage(),
+      //for testing
+      // home: HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  String AccountImage = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (userNameController.text == "Mohammad Usman Asegaf") {
+      setState(() {
+        AccountImage = 'assets/Usman.jpg';
+      });
+    } else if (userNameController.text == "Syamsu Nuryaman") {
+      setState(() {
+        AccountImage = 'assets/Syamsu.jpg';
+      });
+    }
+  }
+
+  List<Widget> _widgetOptions() {
+    return [
+      Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Foto profil sederhana
+                CircleAvatar(
+                  radius: 100, // Ukuran foto
+                  backgroundImage: AssetImage(
+                      AccountImage), // Ganti dengan path gambar lokal
+                ),
+                SizedBox(height: 16), // Spasi antara foto dan nama
+                // Nama pengguna
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Hi mates, I am " + userNameController.text,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      Center(
+        child: Text(
+          'Search',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      Center(
+        child: Text(
+          'Profile',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Navigation Demo'),
+      ),
+      body: _widgetOptions().elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
